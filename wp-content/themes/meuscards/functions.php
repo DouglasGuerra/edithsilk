4df8fa6fcad6e05c40d44341627e2972a10e8aa3
DouglasGuerra <douglas.flurucava@hotmail.com>
2023-10-05 18:24:44 -0300

    ajuste

--- wp-content/themes/meuscards/functions.php
+++ wp-content/themes/meuscards/functions.php
@@ -1,7 +1,7 @@
 <?php
 
 include get_template_directory() . '/inc/functionparts/mc_inqueues.php';
-require get_template_directory() . '/inc/functionparts/mc_functions.php';
+
 
 require get_template_directory() . '/inc/adminparts/optionspage.php';
 
