7011b8e330c685df3e553ddfe605024ad0b316e0
DouglasGuerra <douglas.flurucava@hotmail.com>
2023-10-05 18:29:47 -0300

    ajuste

--- wp-content/themes/meuscards/inc/functionparts/mc_functions.php
+++ wp-content/themes/meuscards/inc/functionparts/mc_functions.php
@@ -15,7 +15,7 @@ function chart($data){
   new Chart(ctx, {
     type: 'bar',
     data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      labels: data[1][labels],
       datasets: [{
         label: '# of Votes',
         data: [12, 19, 3, 5, 2, 3],
