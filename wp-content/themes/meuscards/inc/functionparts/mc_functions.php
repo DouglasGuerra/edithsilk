c314620be7f6529200ad873e175282fdba06f26a
DouglasGuerra <douglas.flurucava@hotmail.com>
2023-10-05 18:52:16 -0300

    ajuste

--- wp-content/themes/meuscards/inc/functionparts/mc_functions.php
+++ wp-content/themes/meuscards/inc/functionparts/mc_functions.php
@@ -4,38 +4,37 @@ function chart($labels, $data){
 ?>
     
     <div>
-  <canvas id="myChart"></canvas>
-</div>
+    <canvas id="myChart"></canvas>
+    </div>
 
-<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
-<script>
-  const ctx = document.getElementById('myChart');
+    <script>
+    const ctx = document.getElementById('myChart');
 
-  new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: <?php echo $labels ?>,
-      datasets: [{
-        label: '# of Votes',
-        data: <?php echo $data ?>,
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+        labels: <?php echo $labels ?>,
+        datasets: [{
+            label: '# of Votes',
+            data: <?php echo $data ?>,
+            borderWidth: 1
+        }]
+        },
+        options: {
+        scales: {
+            y: {
+            beginAtZero: true
+            }
         }
-      }
-    }
-  });
-</script>
+        }
+    });
+    </script>
 
- <?php        
-    echo $data;
-    echo $labels;
-    
-
+ <?php
+         
+    print_r($labels);
+    print_r($data);
 
 }
