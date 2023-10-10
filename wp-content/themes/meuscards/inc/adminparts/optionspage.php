20a1fc93ace031ec3836e36ee36d0ed4a8c20e4f
Wesley Cardoso <cardoso.tads@gmail.com>
2023-10-10 17:38:45 -0400

    Modal Projeto - fix 1

--- wp-content/themes/meuscards/inc/adminparts/optionspage.php
+++ wp-content/themes/meuscards/inc/adminparts/optionspage.php
@@ -85,14 +85,14 @@ function pagina_de_estatisticas ()
 
                     $get_image = get_field('imagem_do_projeto');
 
-                    $html .= '<div class="card p-0" style="width: 18rem; ">' .
+                    $html .= '<div class="card p-0" style="width: 18rem;">' .
                         '<img src="' . $get_image . '" class="card-img-top" alt="...">' .
                         '<div class="card-body">' .
                         '<h5 class="card-title">' . get_the_title() . '</h5>' .
                         '<span class="card-text">' . get_post_status() . '</span>' .
                         '<p class="card-text">' . get_the_excerpt() . '</p>' .
                         '<span class="card-text">' . get_the_date() . '</span>' .
-                        '<button class="open-modal-button" data-post-id="' . get_the_ID() . '">Abrir Modal</button>' .
+                        '<button class="btn btn-primary open-modal-button" data-toggle="modal" data-target="#postModal' . get_the_ID() . '">Abrir Modal</button>' .
                         '</div>' .
                         '</div>';
 
@@ -104,41 +104,38 @@ function pagina_de_estatisticas ()
             endif;
             ?>
 
-            <!-- Modal Genérico -->
-            <div id="genericModal" class="modal">
-                <div class="modal-content">
-                    <span class="close">&times;</span>
-                    <div id="modal-content"></div>
-                </div>
-            </div>
-
-            <script>
-                document.addEventListener('DOMContentLoaded', function () {
-                    var genericModal = document.getElementById('genericModal');
-                    var modalContent = document.getElementById('modal-content');
-                    var closeModalButton = document.querySelector('.close');
-                    var openModalButtons = document.querySelectorAll('.open-modal-button');
-
-                    openModalButtons.forEach(function (button) {
-                        button.addEventListener('click', function () {
-                            var postId = button.getAttribute('data-post-id');
-                            var postContent = 'Conteúdo do modal para o post ID ' + postId; // Personalize conforme necessário
-                            modalContent.innerHTML = postContent;
-                            genericModal.style.display = 'block';
-                        });
-                    });
-
-                    closeModalButton.addEventListener('click', function () {
-                        genericModal.style display = 'none';
-                    });
-
-                    window.addEventListener('click', function (event) {
-                        if (event.target === genericModal) {
-                            genericModal.style.display = 'none';
-                        }
-                    });
-                });
-            </script>
+            <?php
+            $query = new WP_Query($args);
+            if ($query->have_posts()) :
+                while ($query->have_posts()) :
+                    $query->the_post();
+                    ?>
+                    <!-- Modal para cada post -->
+                    <div class="modal fade" id="postModal<?php echo get_the_ID(); ?>" tabindex="-1" role="dialog" aria-labelledby="postModalLabel<?php echo get_the_ID(); ?>" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="postModalLabel<?php echo get_the_ID(); ?>"><?php echo get_the_title(); ?></h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    <!-- Conteúdo do modal para o post -->
+                                    <p><?php echo get_the_excerpt(); ?></p>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                <?php endwhile;
+                wp_reset_postdata();
+            else :
+                echo 'Nenhum projeto encontrado.';
+            endif;
+            ?>
         </div>
     </div>
     <?php
