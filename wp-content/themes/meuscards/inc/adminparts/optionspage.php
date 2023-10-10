ac0ec448b5665d15593406281e1e823b825debe8
Wesley Cardoso <cardoso.tads@gmail.com>
2023-10-10 19:55:16 -0400

    Modal Projeto - fix 4

--- wp-content/themes/meuscards/inc/adminparts/optionspage.php
+++ wp-content/themes/meuscards/inc/adminparts/optionspage.php
@@ -102,11 +102,12 @@ function pagina_de_estatisticas ()
                         <div class="modal-dialog">
                             <div class="modal-content">
                                 <div class="modal-header">
-                                    <h1 class="modal-title fs-5" id="exampleModalLabel"><?= get_the_title() ?>></h1>
+                                    <h1 class="modal-title fs-5" id="exampleModalLabel"><?= get_the_title() ?></h1>
                                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                 </div>
                                 <div class="modal-body">
-                                    ...
+                                    <?= get_the_post_thumbnail() ?>
+                                    <?php the_content() ?>
                                 </div>
                                 <div class="modal-footer">
                                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
