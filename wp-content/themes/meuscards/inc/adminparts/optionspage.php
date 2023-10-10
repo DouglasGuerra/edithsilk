d21148d352660c67347cc9109819924befcd27a3
Wesley Cardoso <cardoso.tads@gmail.com>
2023-10-10 17:35:53 -0400

    Modal Projeto

--- wp-content/themes/meuscards/inc/adminparts/optionspage.php
+++ wp-content/themes/meuscards/inc/adminparts/optionspage.php
@@ -78,7 +78,7 @@ function pagina_de_estatisticas ()
             if ($query->have_posts()) :
                 while ($query->have_posts()) :
                     $query->the_post();
-//
+
                     // Faça algo com os posts, por exemplo, exibi-los
 
                     $html = "";
@@ -86,23 +86,59 @@ function pagina_de_estatisticas ()
                     $get_image = get_field('imagem_do_projeto');
 
                     $html .= '<div class="card p-0" style="width: 18rem; ">' .
-                        '<img src=" '. $get_image . ' " class="card-img-top" alt="...">'.
-                        '<div class="card-body">'.
-                        '<h5 class="card-title"> '. get_the_title() .' </h5>'.
-                        '<span class="card-text"> '. get_post_status() . '</span>'.
-                        '<p class="card-text"> '. get_the_excerpt() . '</p>'.
-                        '<span class="card-text"> '. get_the_date() .'</span>'.
-                        '</div>'.
+                        '<img src="' . $get_image . '" class="card-img-top" alt="...">' .
+                        '<div class="card-body">' .
+                        '<h5 class="card-title">' . get_the_title() . '</h5>' .
+                        '<span class="card-text">' . get_post_status() . '</span>' .
+                        '<p class="card-text">' . get_the_excerpt() . '</p>' .
+                        '<span class="card-text">' . get_the_date() . '</span>' .
+                        '<button class="open-modal-button" data-post-id="' . get_the_ID() . '">Abrir Modal</button>' .
+                        '</div>' .
                         '</div>';
 
                     echo $html;
                 endwhile;
                 wp_reset_postdata(); // Restaura os dados do post original
             else :
-                // Caso não haja posts
-                'Nenhum projeto encontrado.';
+                echo 'Nenhum projeto encontrado.';
             endif;
             ?>
+
+            <!-- Modal Genérico -->
+            <div id="genericModal" class="modal">
+                <div class="modal-content">
+                    <span class="close">&times;</span>
+                    <div id="modal-content"></div>
+                </div>
+            </div>
+
+            <script>
+                document.addEventListener('DOMContentLoaded', function () {
+                    var genericModal = document.getElementById('genericModal');
+                    var modalContent = document.getElementById('modal-content');
+                    var closeModalButton = document.querySelector('.close');
+                    var openModalButtons = document.querySelectorAll('.open-modal-button');
+
+                    openModalButtons.forEach(function (button) {
+                        button.addEventListener('click', function () {
+                            var postId = button.getAttribute('data-post-id');
+                            var postContent = 'Conteúdo do modal para o post ID ' + postId; // Personalize conforme necessário
+                            modalContent.innerHTML = postContent;
+                            genericModal.style.display = 'block';
+                        });
+                    });
+
+                    closeModalButton.addEventListener('click', function () {
+                        genericModal.style display = 'none';
+                    });
+
+                    window.addEventListener('click', function (event) {
+                        if (event.target === genericModal) {
+                            genericModal.style.display = 'none';
+                        }
+                    });
+                });
+            </script>
         </div>
     </div>
     <?php
