d306eceb5b7f759fb7bdbc280691f891557b8881
Wesley Cardoso <cardoso.tads@gmail.com>
2023-10-10 20:33:04 -0400

    Image modal fix - 15

--- wp-content/themes/meuscards/inc/adminparts/optionspage.php
+++ wp-content/themes/meuscards/inc/adminparts/optionspage.php
@@ -97,6 +97,29 @@ function pagina_de_estatisticas ()
                                 Launch demo modal
                             </button>'.
                         '</div>';
+                    ?>
+                    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <?php
+                                    $query->the_post();
+                                    echo '<div class="modal-header">' .
+                                        '<h1 class="modal-title fs-5" id="exampleModalLabel">' . get_the_title() . '</h1>' .
+                                        '<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>' .
+                                    '</div>'.
+                                    // Faça algo com os posts, por exemplo, exibi-los
+                                        '<div class="modal-body">' .
+                                            '<img src="' . $get_image . '">' .
+                                        '</div>';
+                                ?>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    <button type="button" class="btn btn-primary">Save changes</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <?php
                     echo $html;
                 endwhile;
                 wp_reset_postdata(); // Restaura os dados do post original
@@ -106,32 +129,7 @@ function pagina_de_estatisticas ()
             endif;
             ?>
         </div>
-        
-            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <?php
-                            while ($query->have_posts()) :
-                            $query->the_post();
-                            echo '<div class="modal-header">' .
-                                '<h1 class="modal-title fs-5" id="exampleModalLabel">' . get_the_title() . '</h1>' .
-                                '<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>' .
-                            '</div>'.
-                            // Faça algo com os posts, por exemplo, exibi-los
-                                '<div class="modal-body">' .
-                                    '<img src="' . $get_image . '">' .
-                                '</div>';
-
-                            endwhile;
-                        ?>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-primary">Save changes</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+    </div>
     <?php
 
 }
